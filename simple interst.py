3f9ae19fb07c11607fe4ffac1d5a7314a41793a0
shivamngp <114685459+shivamngp@users.noreply.github.com>
2022-10-14 23:51:08 +0530

    Add files via upload

---
+++ simple interst.py
@@ -0,0 +1,8 @@
+i=1
+while i <= 3:
+    p=float(input('enter the value of p'))
+    n=int(input('enter the value of n'))
+    r=float(input('enter the value of r'))
+    si=p*n*r/100
+    print('simple interest = rs.'+ str(si))
+    i = i+1
